602eaee15b5b264b28dcd5b32acc42256bf76a27
SimonSousa25 <simonoliveirasousa02@gmail.com>
2025-12-27 02:09:44 -0300

    Criando o mapeamento JPA para entidade Produto

--- src/main/java/io/github/cursospringboot/produtosapi/model/Produto.java
+++ src/main/java/io/github/cursospringboot/produtosapi/model/Produto.java
@@ -1,9 +1,27 @@
 package io.github.cursospringboot.produtosapi.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 // POJO -> Plain Old Java Object
+
+@Entity
+@Table(name = "produto")
 public class Produto {
 
-    private String id, nome, descricao;
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "preco")
     private Double preco;
 
     public String getId() {
