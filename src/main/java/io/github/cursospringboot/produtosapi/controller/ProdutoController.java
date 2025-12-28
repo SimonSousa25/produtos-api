be32f66714fb0ca9e6f07e23dfeacffb4b980d68
SimonSousa25 <simonoliveirasousa02@gmail.com>
2025-12-27 22:21:08 -0300

    Atualizando os dados de um produto

--- src/main/java/io/github/cursospringboot/produtosapi/controller/ProdutoController.java
+++ src/main/java/io/github/cursospringboot/produtosapi/controller/ProdutoController.java
@@ -36,4 +36,10 @@ public class ProdutoController {
     public void deletarProdutoPorId(@PathVariable("id") String id) {
         produtoRepository.deleteById(id);
     }
+
+    @PutMapping("/{id}")
+    public void atualizarProduto(@PathVariable("id") String id, @RequestBody Produto produto) {
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
 }
